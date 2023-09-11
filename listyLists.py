80f523810aede31e29a05896776ad5f27ca610d4
RyanPWebb <fly20webb@att.net>
2023-09-11 15:39:58 -0500

    Added product function

--- listyLists.py
+++ listyLists.py
@@ -1,6 +1,12 @@
 import math
 
-def listSum(list):
+def listSum(list: int):
     return sum(list)
 
+def listProd(list: int):
+    prod = 1
+    for i in range(0,len(list)):
+        prod *= list[i]
+
+    return prod
 
